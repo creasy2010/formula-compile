0c17bf67aac9f877a8cdd1521b66fa39ff3e4704
zhengchao <chaohfut@163.com>
2020-07-16 11:09:16 +0800

    公式还原测试用例

--- js/src/util.ts
+++ js/src/util.ts
@@ -90,3 +90,121 @@ function traverse(tree:ParserRuleContext,result){
   }
 
 }
+
+export const formulaFormat = (()=>{
+  const format = (ast)=>{
+    let rawFormula = '';
+    if (ast && Object.prototype.toString.call(ast.formulas) === "[object Array]") {
+      ast.formulas.forEach((formula, index)=>{
+        rawFormula += formatAtomicFormula(formula)
+        if (index !== ast.formulas.length-1) {
+          rawFormula += ';'
+        }
+      })
+    }
+    return rawFormula;
+  }
+  const formatAtomicFormula = (obj, newLine=false, space='\t')=>{
+    let rawAtomicFormula = '';
+    if (newLine) {
+      // rawAtomicFormula += '\n' + space;
+      // space += '\t';  //处理缩进层级显示
+    }
+    if(!obj){
+      return rawAtomicFormula;
+    }
+    if (typeof obj === 'string') {
+      return rawAtomicFormula;
+    }
+    const name = obj.name;
+    const type = obj['!'];
+    return rawAtomicFormula + formulaStrategy[type](obj, newLine, space);
+  }
+
+  var formulaStrategy = {
+    FormulaRefTemplateFunction: (obj, newLine, space)=>{
+      return `RefTemplate(${obj.refSheet.value},${obj.refCell.value})`;
+    },
+    FormulaIfFunction: (obj, newLine, space)=>{
+      let rawAtomicFormula = '';
+      rawAtomicFormula += `IF(`;
+      const leftExp = formatAtomicFormula(obj.condition.content, false, space);
+      const thenExp = formatAtomicFormula(obj.thenStatement.content, true, space);
+      const elseExp = formatAtomicFormula(obj.elseStatement.content, true, space);
+      rawAtomicFormula += `${leftExp},${thenExp},${elseExp})`;
+      return rawAtomicFormula;
+    },
+    FormulaBracketExpress: (obj, newLine, space)=>{
+      let rawAtomicFormula = '';
+      rawAtomicFormula += `(`;
+      obj.express.forEach(exp=>{
+        rawAtomicFormula += formatAtomicFormula(exp, false, space);
+      })
+      rawAtomicFormula += `)`;
+      return rawAtomicFormula;
+    },
+    FormulaBinaryOperationExpress: (obj, newLine, space)=>{
+      let rawAtomicFormula = '';
+      rawAtomicFormula += `${formatAtomicFormula(obj.leftExp, false, space)}`;
+      if(obj.operate){
+        if (obj.operate.value === '+' || obj.operate.value === '-' || obj.operate.value === '*' || obj.operate.value === '/') {
+          // rawAtomicFormula += '\n\t';
+        }
+        rawAtomicFormula += `${obj.operate.value}`;
+      }
+      if(obj.rightExp){
+        rawAtomicFormula += `${formatAtomicFormula(obj.rightExp, false, space)}`;
+      }
+      return rawAtomicFormula;
+    },
+    FormulaParamString: (obj, newLine, space)=>{
+      return `'${obj.value}'`;
+    },
+    FormulaParamConst: (obj, newLine, space)=>{
+      return `${obj.value}`;
+    },
+    FormulaParamNum: (obj, newLine, space)=>{
+      return `${obj.value}`;
+    },
+    FormulaExpress: (obj, newLine, space)=>{
+      let rawAtomicFormula = '';
+      obj.express.forEach(exp=>{
+        rawAtomicFormula += formatAtomicFormula(exp, newLine, space);
+      })
+      return rawAtomicFormula;
+    },
+    FormulaCELLLoc: (obj, newLine, space)=>{
+      let rawAtomicFormula = '';
+      if (obj.sheetName) {
+        rawAtomicFormula += `{${obj.sheetName}}!`;
+      }
+      if (obj.cellLoc) {
+        rawAtomicFormula += `[${obj.cellLoc}]`;
+      }
+      return rawAtomicFormula;
+    },
+    FormulaFunction: (obj, newLine,space)=>{
+      let rawAtomicFormula = '';
+      const name = obj.name;      
+      if (typeof name === 'string') {
+        rawAtomicFormula += `${name}(`;
+        obj.params.forEach((param, paramIndex)=>{
+            const paramRes = formatAtomicFormula(param.content, false, space);
+            if (obj.params.length === 1) {
+              rawAtomicFormula += `${paramRes}`;
+            }else if (paramIndex===0) {
+              rawAtomicFormula += `${paramRes},`;
+            }else if(paramIndex === obj.params.length -1){
+              rawAtomicFormula += `${paramRes}`;
+            }else{
+              rawAtomicFormula += `${paramRes},`;
+            }
+        })
+        rawAtomicFormula += `)`;
+      }
+      return rawAtomicFormula;
+    }
+  }
+
+  return format;
+})()
