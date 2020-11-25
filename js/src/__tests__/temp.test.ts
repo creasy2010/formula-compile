8b9bdd0950d301628c9105649a63bf8a40356102
yangxiaodong <yangxiaodong@qianmi.com>
2020-11-26 04:52:19 +0800

    add; 添加 assign的单元测试

--- js/src/__tests__/temp.test.ts
+++ js/src/__tests__/temp.test.ts
@@ -13,10 +13,12 @@ toggleRangeInfo();
 
 describe('临时测试', () => {
 
+
   it('单个用例',async()=>{
     let visitor  = new FormulaTSVisitor();
-    // let formula=`IF([E5]!=0,[D5]/([E5]==0 ? 1 : [E5]),0)`;
-    let formula=`$KMDZID$=GET_TOP_XJKMDZID($ZTDM$, $KJND_B$);UDEF_PZ_XJLL($ZTDM$,$KMDZID$,$KJND_B$,$KJQJ_B$,$KJND_E$,$KJQJ_E$,'1','主营业务收入|SR/-|主营业务收入|ZC/+|其他业务收入|SR/-|其他业务收入|ZC/+|应收票据|SR/-|应收票据|ZC/+|应收账款|SR/-|应收账款|ZC/+|预收账款|SR/-|预收账款|ZC')`;
+    let formula=`IF(11,'2000-00-00',aa(1,2,3,'ZCRQ'))`;//ok
+    // let formula=`IF(11,"2000-00-00",aa(1,2,3,'ZCRQ'))`;
+    // let formula=`$KMDZID$=GET_TOP_XJKMDZID($ZTDM$, $KJND_B$);UDEF_PZ_XJLL($ZTDM$,$KMDZID$,$KJND_B$,$KJQJ_B$,$KJND_E$,$KJQJ_E$,'1','主营业务收入|SR/-|主营业务收入|ZC/+|其他业务收入|SR/-|其他业务收入|ZC/+|应收票据|SR/-|应收票据|ZC/+|应收账款|SR/-|应收账款|ZC/+|预收账款|SR/-|预收账款|ZC')`;
 
     let ast  =await visitor.toAst(formula);
     console.log(JSON.stringify(ast,null,2));
